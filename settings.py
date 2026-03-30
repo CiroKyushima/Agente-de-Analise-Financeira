8c2e00edcc43bb4ae1199cf70904373d9fcab0f4
Ciro Kyushima <132959184+CiroKyushima@users.noreply.github.com>
2026-03-30 10:15:22 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ class Settings:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     BOT_NAME = os.getenv("BOT_NAME", "BolsaIA_Bot")
-    MODEL_NAME = "gpt-4-turbo-preview"
+    MODEL_NAME = "gpt-4o-mini"
 
 settings = Settings()
